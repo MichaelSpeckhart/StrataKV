d2d8c64d03dfa1226fc6aeee34cd041039fb48c1
MichaelSpeckhart <msspeck394@gmail.com>
2025-12-27 22:36:16 -0500

    Initial Commit

---
+++ Project.md
@@ -0,0 +1,6 @@
+# Project Milestones
+Milestone 1: Simple Table with One Lock
+
+## Tasks
+- [ ] Define KeyValue type
+- [ ]
---
+++ include/SimpleHashMap.h
@@ -0,0 +1,15 @@
+#ifndef STRATADB_SIMPLEHASHMAP_H
+#define STRATADB_SIMPLEHASHMAP_H
+
+
+#include <vector>
+
+template<typename K, typename V>
+class SimpleHashMap {
+
+
+};
+
+
+
+#endif //STRATADB_SIMPLEHASHMAP_H
